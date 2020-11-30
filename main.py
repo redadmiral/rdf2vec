f5921c1bca746ebcfd0b2081560dfdeb04d1680f
Marco Lehner <marco-lehner@posteo.net>
2020-11-30 22:28:12 +0100

    add embedding training function. Add logging.

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+from pyrdf2vec import RDF2VecTransformer
+from pyrdf2vec.embedders import Word2Vec
 from pyrdf2vec.graphs import KG
 from pyrdf2vec.samplers import UniformSampler
-from pyrdf2vec.walkers import RandomWalker
-from pyrdf2vec import RDF2VecTransformer
+from pyrdf2vec.walkers import RandomWalker, Walker
+import numpy as np
+import logging
+
+logging.basicConfig(filename="rdf2vec.log", level = logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 # Define the label predicates, all triples with these predicates
 # will be excluded from the graph
+logging.info("Read in knowledge graph.")
 label_predicates = []
-kg = KG(location="samples/mutag/mutag.owl", label_predicates=label_predicates)
+kg = KG(location="data/dbp_graph_sample.ttl", label_predicates=label_predicates)
 
+logging.info("Create walkers and transformers.")
 walkers = [RandomWalker(4, 5, UniformSampler())]
+transformer = RDF2VecTransformer(Word2Vec(sg=1), walkers=walkers)
 
-transformer = RDF2VecTransformer(walkers=[walkers], sg=1)
+logging.info("Read in entities.")
 # Entities should be a list of URIs that can be found in the Knowledge Graph
+entities = list(np.load("data/entities.npy", allow_pickle=True))
+logging.info("Calculate embeddings.")
 embeddings = transformer.fit_transform(kg, entities)
+logging.info("Write embeddings to disk.")
+np.save("data/embeddings.npy", embeddings)
+logging.info("Finished job.")
--- parse_ents.py
+++ parse_ents.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import numpy as np
+import logging
 
-graph = pd.read_csv("data/dbp_graph.ttl", sep=" ")
+logging.basicConfig(filename="rdf2vec.log", level = logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("Create DataFrame...")
+graph = pd.read_csv("data/dbp_graph_sample_head.ttl", sep=" ")
 graph.columns = ["sub", "pred", "obj", "dot"]
 graph = graph.drop(["pred", "dot"], axis = 1)
 
+logging.info("Filter out entities...")
 entities: pd.Series = graph["sub"]
 entities = entities.append(graph["obj"])
 
+logging.info("Replace pointy brackets...")
 entities = entities.str.replace("<", "").str.replace(">", "")
+logging.info("Filter non-dpb resources...")
 entities = entities[entities.str.startswith("http://de.dbpedia.org/resource/")]
+logging.info("Make unique...")
 entities = entities.unique()
-
+logging.info("Write to disk...")
 np.save("data/entities.npy",entities)
+logging.info("Finished job.")
